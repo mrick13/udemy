09a9fc60da86f343a3896f90f3b652f6ca3ec9b9
mrick13 <aymeric.baud@icloud.com>
2024-01-16 11:14:36 +0100

    ajout bordure + detail list et list pokemon

---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+
+})
+export class ListPokemonComponent {
+  
+  pokemonList: Pokemon[] = POKEMONS;
+
+
+}
